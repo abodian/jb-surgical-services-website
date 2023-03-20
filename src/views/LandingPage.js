f18c70cf37efe61a8dad0c2a2e89d3e1639146e5
abodian <ajnbodian@gmail.com>
2023-03-20 22:15:15 +0000

    pre our services commit

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -2,11 +2,6 @@ import React from "react";
 
 // reactstrap components
 import {
-  Button,
-  Input,
-  InputGroupAddon,
-  InputGroupText,
-  InputGroup,
   Container,
   Row,
   Col
@@ -24,8 +19,6 @@ import JemSarahWalk from '../assets/img/jemsarahwalk.jpg'
 require('./LandingPage.css')
 
 function LandingPage() {
-  const [firstFocus, setFirstFocus] = React.useState(false);
-  const [lastFocus, setLastFocus] = React.useState(false);
   
   React.useEffect(() => {
     document.body.classList.add("landing-page");
@@ -87,7 +80,7 @@ function LandingPage() {
         </div>
         <div className="text-center">
           <Container className="ml-auto mr-auto text-center">
-          <h2 className="title" id="team-title">Here is our team</h2>
+          <h1 id='our-team-title'>Our Team</h1>
           <Row>
             <Col md="6">
               <div className="team left-column-profile">
@@ -151,69 +144,25 @@ function LandingPage() {
             </Col>
           </Row>
         </Container>
+        <div className="section section-about-us">
+          <Container className="ml-auto mr-auto text-center">
+          <div className='about-us-title'><h1 id='our-services-title'>Our Services</h1></div> 
+            <div className="separator separator-primary"></div>
+            <div className="section-story-overview">
+              
+            </div>
+          </Container>
+        </div>
       </div>
+
 
         <div className="section section-contact-us text-center">
           <Container>
-            <h2 className="title">Want to work with us?</h2>
-            <p className="description">Your project is very important to us.</p>
+            <h2 className="title">Contact Us</h2>
+            <p className="description"></p>
             <Row>
               <Col className="text-center ml-auto mr-auto" lg="6" md="8">
-                <InputGroup
-                  className={
-                    "input-lg" + (firstFocus ? " input-group-focus" : "")
-                  }
-                >
-                  <InputGroupAddon addonType="prepend">
-                    <InputGroupText>
-                      <i className="now-ui-icons users_circle-08"></i>
-                    </InputGroupText>
-                  </InputGroupAddon>
-                  <Input
-                    placeholder="First Name..."
-                    type="text"
-                    onFocus={() => setFirstFocus(true)}
-                    onBlur={() => setFirstFocus(false)}
-                  ></Input>
-                </InputGroup>
-                <InputGroup
-                  className={
-                    "input-lg" + (lastFocus ? " input-group-focus" : "")
-                  }
-                >
-                  <InputGroupAddon addonType="prepend">
-                    <InputGroupText>
-                      <i className="now-ui-icons ui-1_email-85"></i>
-                    </InputGroupText>
-                  </InputGroupAddon>
-                  <Input
-                    placeholder="Email..."
-                    type="text"
-                    onFocus={() => setLastFocus(true)}
-                    onBlur={() => setLastFocus(false)}
-                  ></Input>
-                </InputGroup>
-                <div className="textarea-container">
-                  <Input
-                    cols="80"
-                    name="name"
-                    placeholder="Type a message..."
-                    rows="4"
-                    type="textarea"
-                  ></Input>
-                </div>
-                <div className="send-button">
-                  <Button
-                    block
-                    className="btn-round"
-                    color="info"
-                    href="#pablo"
-                    onClick={(e) => e.preventDefault()}
-                    size="lg"
-                  >
-                    Send Message
-                  </Button>
-                </div>
+                Contact Us Section Here
               </Col>
             </Row>
           </Container>
