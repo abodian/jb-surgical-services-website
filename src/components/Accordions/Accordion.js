59c6c4bc33ebce341597826ccc3d224bdf9b156b
abodian <ajnbodian@gmail.com>
2023-03-22 21:22:51 +0000

    fade animation to services dropdown

--- src/components/Accordions/Accordion.js
+++ src/components/Accordions/Accordion.js
@@ -1,18 +1,28 @@
 import React, { useState } from 'react';
 import OurServicesCard from '../Cards/OurServicesCard.js'
+import Radium, {StyleRoot} from 'radium';
+import { fadeIn } from 'react-animations'
 
 
 const Accordion = ({ title, content}) => {
   const [isActive, setIsActive] = useState(false);
 
+  const styles = {
+    fadeIn: {
+      animation: 'x 1s',
+      animationName: Radium.keyframes(fadeIn, 'fadeIn')
+    }
+  }
+
   return (
+    <StyleRoot>
     <div className="accordion-item">
       <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
         <div className='accordion-title-text'>{title}</div>
         <div className='is-open'>{isActive ? '-' : '+'}</div>
       </div>
       {isActive && 
-        <div className="accordion-content">
+        <div className="accordion-content" style={{...styles.fadeIn}}>
           <div className='our-services-card'>
             {Object.entries(content).map(([key, value]) => (
               <OurServicesCard serviceTitle={key} serviceContent={value} />
@@ -21,6 +31,7 @@ const Accordion = ({ title, content}) => {
         </div>
       }
     </div>
+    </StyleRoot>
   );
 };
 
